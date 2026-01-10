f8d64999714e4d96350123b7b0bfb95d102cce77
Alexandre Costa <alexandre.costa@furg.br>
2026-01-10 17:13:31 -0300

    renomeia função list_acoes para list_acoes_all para maior clareza e atualiza referência no serviço fundamentus

--- services/fundamentus.py
+++ services/fundamentus.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from repositories.acao import list_acoes as repo_list_acoes
+from repositories.acao import list_acoes_all
 
 def list_acoes(db: Session, skip: int = 0, limit: int = 10):
-    return repo_list_acoes(db, skip, limit)
+    return list_acoes_all(db, skip, limit)
