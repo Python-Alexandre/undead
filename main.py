1befbd7b05a9cacee5b027fd8c4138713cc13897
Alexandre Costa <alexandre.costa@furg.br>
2026-01-10 17:14:54 -0300

    refatora a inicialização da aplicação para usar um gerenciador de contexto assíncrono e simplifica o gerenciamento de eventos de startup

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from api.v1.api import api_router
 from db.init_db import init_db
 
-app = FastAPI(title="API FastAPI Completa")
-
-@app.on_event("startup")
-def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
     init_db()
+    yield
+    # Shutdown (se necessário)
+
+app = FastAPI(title="API FastAPI Completa", lifespan=lifespan)
 
 app.include_router(api_router)
